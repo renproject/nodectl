063c916486bf586a5107e52387909c2722b3248c
Rahul Ghangas <rahulghangas0@gmail.com>
2021-09-28 14:00:55 +0530

    feat: add network flag to update command

--- nodectl.go
+++ nodectl.go
@@ -83,7 +83,7 @@ func App() *cli.App {
 		{
 			Name:  "update",
 			Usage: "Update your Darknode to the latest version",
-			Flags: []cli.Flag{TagsFlag, VersionFlag},
+			Flags: []cli.Flag{TagsFlag, VersionFlag, NetworkFlag},
 			Action: func(c *cli.Context) error {
 				return UpdateDarknode(c)
 			},
