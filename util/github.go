b2a874b0eb5e64e14164bba2f1c064816cc8bb49
tok-kkk <yunshi@renproject.io>
2022-03-18 12:36:18 +1100

    update function to fetch the latest release name according to the release naming change

--- util/github.go
+++ util/github.go
@@ -2,13 +2,11 @@ package util
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"net/http"
 	"os"
 	"regexp"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/google/go-github/v36/github"
@@ -32,77 +30,14 @@ func GithubClient(ctx context.Context) *github.Client {
 	return github.NewClient(client)
 }
 
-// LatestStableRelease checks the node release repo and return the version of the latest release.
-func LatestStableRelease() (string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	latest, err := version.NewVersion("0.0.0")
-	if err != nil {
-		return "", err
-	}
-	client := GithubClient(ctx)
-	opts := &github.ListOptions{
-		Page:    0,
-		PerPage: 100,
-	}
-
-	for {
-		releases, response, err := client.Repositories.ListReleases(ctx, "renproject", "darknode-release", opts)
-		if err != nil {
-			return "", err
-		}
-
-		// Verify the status code is 200.
-		if err := VerifyStatusCode(response.Response, http.StatusOK); err != nil {
-			return "", err
-		}
-
-		// Find the latest stable production release
-		stableVer, err := regexp.Compile("^v?[0-9]+\\.[0-9]+\\.[0-9]+$")
-		if err != nil {
-			return "", err
-		}
-		for _, release := range releases {
-			if stableVer.MatchString(*release.TagName) {
-				ver, err := version.NewVersion(*release.TagName)
-				if err != nil {
-					continue
-				}
-				if ver.GreaterThan(latest) {
-					latest = ver
-				}
-			}
-		}
-
-		if response.NextPage == 0 {
-			break
-		}
-		opts.Page = response.NextPage
-	}
-
-	if latest.String() == "0.0.0" {
-		return "", errors.New("cannot find any stable release")
-	}
-
-	return latest.String(), nil
-}
-
 func LatestRelease(network multichain.Network) (string, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
-	max := 0
+	reg := regexp.MustCompile("^(\\d+.\\d+(.\\d+){0,1})-(mainnet|testnet|devnet)(\\d+)$")
+	maxIndex := 0
+	maxVersion, _ := version.NewVersion("0.0.0")
 	tag := ""
-	prefix := ""
-	switch network {
-	case multichain.NetworkMainnet:
-		prefix = "0.4-mainnet"
-	case multichain.NetworkTestnet:
-		prefix = "0.4-testnet"
-	case multichain.NetworkDevnet:
-		prefix = "0.4-devnet"
-	}
 
 	client := GithubClient(ctx)
 	opts := &github.ListOptions{
@@ -122,13 +57,35 @@ func LatestRelease(network multichain.Network) (string, error) {
 
 		// Find the latest release tag for the given network
 		for _, release := range releases {
-			if strings.HasPrefix(*release.TagName, prefix) {
-				num, err := strconv.Atoi(strings.TrimPrefix(*release.TagName, prefix))
-				if err != nil {
-					continue
-				}
-				if num > max {
-					max = num
+			// Parse version, network, index from the release tag name
+			// i.e. "0.4.10-mainnet12"  -> ["0.4.10-mainnet12", "0.4.10", ".10", "mainnet", "12"]
+			matches := reg.FindStringSubmatch(*release.TagName)
+			if len(matches) != reg.NumSubexp()+1 {
+				continue
+			}
+			releaseVersion, err := version.NewVersion(matches[1])
+			if err != nil {
+				continue
+			}
+			releaseNetwork := multichain.Network(matches[3])
+			releaseIndex, err := strconv.Atoi(matches[4])
+			if err != nil {
+				continue
+			}
+
+			// Continue if it's not a different network
+			if releaseNetwork != network {
+				continue
+			}
+
+			// Compare the version and index
+			if releaseVersion.GreaterThan(maxVersion) {
+				maxVersion = releaseVersion
+				maxIndex = releaseIndex
+				tag = *release.TagName
+			} else if releaseVersion.Equal(maxVersion) {
+				if releaseIndex > maxIndex {
+					maxIndex = releaseIndex
 					tag = *release.TagName
 				}
 			}
